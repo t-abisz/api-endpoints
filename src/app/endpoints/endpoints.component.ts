02344c0577c6ddfe6bc6c4d3bba8576a7ee28b03
Pawel <pawel.tabisz@sciamus.com>
2020-07-01 13:50:26 +0200

    adding requests and response

--- src/app/endpoints/endpoints.component.ts
+++ src/app/endpoints/endpoints.component.ts
@@ -11,5 +11,4 @@ export class EndpointsComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
--- src/app/endpoints/list-api/list-api.component.ts
+++ src/app/endpoints/list-api/list-api.component.ts
@@ -14,5 +14,4 @@ export class ListApiComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
